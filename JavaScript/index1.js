25ac6cea11833b7a349ed1d22fbcdbd3eb03c9be
VritantJoshi <joshivritant231@gmail.com>
2025-07-09 21:55:24 +0000

    commit

---
+++ JavaScript/index1.js
@@ -0,0 +1,27 @@
+let a = 10;
+console.log(a);
+
+//let a = 20; // let can be reassigned but can't be redeclared within same block scope and can't be redefined.
+
+a = "vishu";
+a = true;
+a = null;
+
+console.log(a);
+
+
+// const b = 25; //const can neither be redefined nor reassigned.
+// console.log(b);
+
+// b = 20;
+// const b = 30;
+
+ let marks = 20.3242;
+ marks = "vj";
+ marks = true;
+ marks = null;
+
+//let marks;
+console.log(typeof(marks));
+
+
---
+++ JavaScript/loopsAndStrings.js
@@ -0,0 +1,62 @@
+let n = 10;
+
+// for(let i=1;i<=n;i++){
+//     console.log(i);
+    
+// }
+
+// for(let i=5;i>=1;i--){
+//     console.log(i);
+//     if(i==3){
+//         break;
+//     }
+// }
+
+// for(let i=5;i>=1;i--){
+//     if(i==3){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }    
+// }
+
+
+// for(let i=1;i<=10;i++){
+//     if(i==6){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// while (n>0) {
+
+//     console.log(n);
+//     n--;
+    
+// }
+
+let i = 1;
+
+// while(i<6){
+
+//     if(i==3){
+//         i++;
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+i=1;
+
+// do{
+
+// console.log(i);
+// i++
+
+// }while (i<=10);
