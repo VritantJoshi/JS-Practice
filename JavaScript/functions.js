84005dbe31e156ed93297e8b783fb5f4a3d4d5ca
VritantJoshi <joshivritant231@gmail.com>
2025-07-10 05:02:04 +0000

    commit;

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,4 +1,49 @@
-function saymyname() {
-    console.log("Vritant Joshi");
+// function saymyname() {
+//     console.log("Vritant Joshi");
     
+// }
+
+// saymyname();
+
+// function printCounting() {
+
+//     for(let i=1;i<=20;i++){
+//         console.log(i);        
+//     }
+    
+// }
+
+// printCounting();
+
+
+// function printNum(n){
+//     console.log(n);
+    
+// }
+
+// printNum(5);
+
+
+// function printNums(n1,n2){
+//     console.log(n1,n2);
+    
+// }
+
+// printNums(5,8);
+
+
+// function average(n1,n2){
+//     let avg = (n1+n2)/2;
+//     console.log(avg);
+// }
+
+// average(5,3);
+
+
+function getSum(n1,n2,n3){
+    let sum = n1+n2+n3;
+    return sum;
 }
+
+let ans = getSum(22,44,22);
+console.log(ans);
