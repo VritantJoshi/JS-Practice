d7499c17d8b0a1a2827766a64427bb273de103ea
VritantJoshi <joshivritant231@gmail.com>
2025-07-10 05:46:01 +0000

    commit;

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -40,10 +40,70 @@
 // average(5,3);
 
 
-function getSum(n1,n2,n3){
-    let sum = n1+n2+n3;
-    return sum;
+// function getSum(n1,n2,n3){
+//     let sum = n1+n2+n3;
+//     return sum;
+// }
+
+// let ans = getSum(22,44,22);
+// console.log(ans);
+
+// function getmyname(firstName, lastName){
+
+//     //let fullName = firstName + " " + lastName;
+//     let fullName = firstName + lastName;
+//     return fullName;
+//     // unreachable statements
+//     // let a = 10;
+//     // let b = 15;
+//     // let sum = a+b;
+//     // console.log(sum);
+     
+// }
+
+// console.log(getmyname("Vritant", "Joshi"));
+// let name = getmyname("Vritant", "Joshi");
+// let name = getmyname(7, 7);
+// console.log(name);
+
+
+// function getMultiplication(a,b){
+//     return a*b;
+// }
+
+// console.log(getMultiplication(2,10));
+
+// let solve = function(a,b){
+//     return a*b;
+// }
+
+// console.log(solve(2,20));
+
+// function square(n){
+//     // return n*n;
+//     return n**2;
+// }
+
+// console.log(square(5));
+
+// let square = function(n){
+//     return n**2;
+// }
+
+// console.log(square(5));
+
+
+//------------------ARROW FUNCTION-------------------//
+
+// let exp = function (x,y){
+//     return x**y;
+// }
+
+// console.log(exp(2,5));
+
+
+let exp = (x,y) => {
+    return x**y;
 }
 
-let ans = getSum(22,44,22);
-console.log(ans);
+console.log(exp(2,5));
