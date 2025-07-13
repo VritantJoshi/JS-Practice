62af2536b78c4ac02166101d56757f58cbef5a04
VritantJoshi <joshivritant231@gmail.com>
2025-07-13 21:11:19 +0000

    commit

--- JavaScript/HoistingAndFunctionCalls.js
+++ JavaScript/HoistingAndFunctionCalls.js
@@ -1,11 +1,19 @@
 // Hoisting is the process which shifts the variable declaration or function declaration to the top of their scope.
 
-sayMyName( "Vritant");
+// sayMyName( "Vritant");
 
-function sayMyName(finalName){  //Let's assume this function is part of an imaginary block, According to Function Hoisting even if a function call is given before is declaration, it is shifted to the top of it's scope at the time of compilation preventing the code to cause errors.
-    console.log(finalName);
+// function sayMyName(finalName){  //Let's assume this function is part of an imaginary block, According to Function Hoisting even if a function call is given before is declaration, it is shifted to the top of it's scope at the time of compilation preventing the code to cause errors.
+//     console.log(finalName);
     
-}
+// }
+
+
+// sayHello();
+
+// let sayHello = function() {     //If we use function expression to declare and define a function, Function Hoisting will not be possible and it will give reference error.
+//     console.log("Hello world");
+    
+// }
 
 console.log(age);
 //  gives undefined. This is because in case of function hoisting the whole function(declaration + defination) shifts to the top of the scope leaving its logic intact, but in case of variable hoisting, when the variable declaration is moved to the top it's defination is left behind and the variable has no given value, so it gives undefined as output.
@@ -15,5 +23,21 @@ var age = 25;
 console.log(age);   //25(Normal execution)
 
 
+// Class Hoisting
 
-// sayMyName("Vritant");
+// const o1 = new Human();
+
+// class Human{    //Class Hoisting is also not possible since it will give reference error.
+
+// }
+
+
+
+
+// Function Call Stack
+
+
+
+
+
+
