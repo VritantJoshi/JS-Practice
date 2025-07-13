0e2373ed7fcc6206a304203244697766b4144da4
VritantJoshi <joshivritant231@gmail.com>
2025-07-13 22:56:59 +0000

    commit

---
+++ JavaScript/ClassesAndDefaultParametersInFunctions.js
@@ -0,0 +1,33 @@
+//
+class Human{
+    //properties(state)
+    age = 13;       //public by default.
+    #weight = 40;   //"#" makes a member private.
+    //In order to get values of private members outside their class we've to use getter and setter methods.
+    height = 140;
+
+    //behaviour(functionality)
+    walking(){
+        console.log("I am walking", this.#weight);  //By using "this"/Current Working Object functionality we can access the value of private members even outside class.
+    }
+
+    running(){
+        console.log("I am running");
+    }
+
+    get fetchWeight(){
+        return this.#weight;
+    }
+
+    set modifyWeight(val){
+        this.#weight = val;
+    }
+
+}
+
+let obj = new Human();
+console.log(obj.age);
+// console.log(obj.#weight);    //gives error since weight is set private and cannot be accessed outside class directly.
+
+
+obj.walking();
