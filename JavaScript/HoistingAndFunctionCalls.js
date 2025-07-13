ce27b34bd1c484ab5b98a198e5c65b679f60661f
VritantJoshi <joshivritant231@gmail.com>
2025-07-13 21:03:28 +0000

    commit

--- JavaScript/HoistingAndFunctionCalls.js
+++ JavaScript/HoistingAndFunctionCalls.js
@@ -9,7 +9,7 @@ function sayMyName(finalName){  //Let's assume this function is part of an imagi
 
 console.log(age);
 //  gives undefined. This is because in case of function hoisting the whole function(declaration + defination) shifts to the top of the scope leaving its logic intact, but in case of variable hoisting, when the variable declaration is moved to the top it's defination is left behind and the variable has no given value, so it gives undefined as output.
-//  variable hoisting can only be done with var, this is because "var" can be redeclared and reassigned unlike "let" and "const".
+//  variable hoisting can only be done with var, this is because "var" can be redeclared and reassigned unlike "let" and "const". In case of "let and const" we will get reference error.
 
 var age = 25;
 console.log(age);   //25(Normal execution)
